155b779782cbf0dbd18f4ce5f4a98b87a27348b7
jdufner <jdufner@99c627dd-ee85-4370-9803-91ec055755ba>
2010-04-01 20:46:15 +0000

    JavaScriptGenerator für Erzeugung von Javascript-Sudokus, die in die HTML-Seite eingebaut werden können.

--- trunk/generator/src/de/jdufner/sudoku/generator/service/PdfGeneratorService.java
+++ trunk/generator/src/de/jdufner/sudoku/generator/service/PdfGeneratorService.java
@@ -2,24 +2,24 @@
 
 /*
  * Gudoku (http://sourceforge.net/projects/gudoku)
- * Sudoku-Implementierung auf Basis des Google Webtoolkit 
- * (http://code.google.com/webtoolkit/). Die Lösungsalgorithmen in Java laufen 
+ * Sudoku-Implementierung auf Basis des Google Webtoolkit
+ * (http://code.google.com/webtoolkit/). Die Lösungsalgorithmen in Java laufen
  * parallel. Die Sudoku-Rätsel werden mittels JDBC in einer Datenbank
  * gespeichert.
- * 
+ *
  * Copyright (C) 2008 Jürgen Dufner
  *
- * Dieses Programm ist freie Software. Sie können es unter den Bedingungen der 
- * GNU General Public License, wie von der Free Software Foundation 
- * veröffentlicht, weitergeben und/oder modifizieren, entweder gemäß Version 3 
+ * Dieses Programm ist freie Software. Sie können es unter den Bedingungen der
+ * GNU General Public License, wie von der Free Software Foundation
+ * veröffentlicht, weitergeben und/oder modifizieren, entweder gemäß Version 3
  * der Lizenz oder (nach Ihrer Option) jeder späteren Version.
  *
- * Die Veröffentlichung dieses Programms erfolgt in der Hoffnung, daß es Ihnen 
- * von Nutzen sein wird, aber OHNE IRGENDEINE GARANTIE, sogar ohne die 
- * implizite Garantie der MARKTREIFE oder der VERWENDBARKEIT FÜR EINEN 
+ * Die Veröffentlichung dieses Programms erfolgt in der Hoffnung, daß es Ihnen
+ * von Nutzen sein wird, aber OHNE IRGENDEINE GARANTIE, sogar ohne die
+ * implizite Garantie der MARKTREIFE oder der VERWENDBARKEIT FÜR EINEN
  * BESTIMMTEN ZWECK. Details finden Sie in der GNU General Public License.
  *
- * Sie sollten ein Exemplar der GNU General Public License zusammen mit diesem 
+ * Sie sollten ein Exemplar der GNU General Public License zusammen mit diesem
  * Programm erhalten haben. Falls nicht, siehe <http://www.gnu.org/licenses/>.
  *
  */
@@ -34,14 +34,15 @@ import java.util.List;
 
 import com.lowagie.text.DocumentException;
 
-import de.jdufner.sudoku.common.board.Sudoku;
 import de.jdufner.sudoku.common.factory.SudokuFactory;
 import de.jdufner.sudoku.dao.SudokuDao;
 import de.jdufner.sudoku.dao.SudokuData;
 import de.jdufner.sudoku.generator.pdf.PdfPrinter;
 import de.jdufner.sudoku.generator.service.PdfGeneratorConfiguration.Page;
-import de.jdufner.sudoku.generator.text.ApproachPrinter;
+import de.jdufner.sudoku.generator.text.ApproachFilePrinter;
+import de.jdufner.sudoku.generator.text.JavascriptGenerator;
 import de.jdufner.sudoku.solver.service.ExtendedSolver;
+import de.jdufner.sudoku.solver.service.Solution;
 
 /**
  * 
@@ -55,10 +56,12 @@ public final class PdfGeneratorService {
 
   private SudokuDao sudokuDao;
   private PdfPrinter pdfPrinter;
-  private ApproachPrinter approachPrinter;
+  private ApproachFilePrinter approachFilePrinter;
   private ExtendedSolver solver;
 
   public void generate(PdfGeneratorConfiguration config) throws DocumentException, IOException {
+    final String date = FORMATTER.format(new Date());
+    approachFilePrinter.openFile(date);
     final List<SudokuData> allSudokuQuests = new ArrayList<SudokuData>();
     for (Page page : config.getPages()) {
       List<SudokuData> sudokus = getSudokuDao().findSudokus(config.getSize(), page.getLevel(), page.getNumber(),
@@ -66,27 +69,33 @@ public final class PdfGeneratorService {
       allSudokuQuests.addAll(sudokus);
     }
     final List<SudokuData> allSudokuResults = new ArrayList<SudokuData>(allSudokuQuests.size());
+    int index = 0;
     for (SudokuData sudokuData : allSudokuQuests) {
       SudokuData sudokuData2 = new SudokuData();
-      Sudoku result = solver.solve(SudokuFactory.buildSudoku(sudokuData.getSudokuAsString()));
+      Solution solution = solver.getSolution(SudokuFactory.buildSudoku(sudokuData.getSudokuAsString()));
       sudokuData2.setId(sudokuData.getId());
       sudokuData2.setLevel(sudokuData.getLevel());
       sudokuData2.setSize(sudokuData.getSize());
-      sudokuData2.setSudokuAsString(result.toString());
-      approachPrinter.print(sudokuData.getId());
+      sudokuData2.setSudokuAsString(solution.getResult().toString());
+      if (index != 0) {
+        approachFilePrinter.print(",");
+      }
+      approachFilePrinter.print(JavascriptGenerator.toJavascript(sudokuData.getId(), solution));
       allSudokuResults.add(sudokuData2);
+      index++;
     }
 
     final List<SudokuData> allSudokus = new ArrayList<SudokuData>(allSudokuQuests.size() * 2);
     allSudokus.addAll(allSudokuQuests);
     allSudokus.addAll(allSudokuResults);
 
-    getPdfPrinter().print(allSudokus, "C:\\tmp\\Sudoku_" + FORMATTER.format(new Date()) + ".pdf");
+    getPdfPrinter().print(allSudokus, "C:\\tmp\\Sudoku_" + date + ".pdf");
 
     Date now = new Date();
     for (SudokuData sudoku : allSudokuQuests) {
       getSudokuDao().updatePrintedAt(sudoku.getId(), now);
     }
+    approachFilePrinter.closeAndCompressFile();
   }
 
   //
@@ -117,12 +126,12 @@ public final class PdfGeneratorService {
     this.solver = solver;
   }
 
-  public ApproachPrinter getApproachPrinter() {
-    return approachPrinter;
+  public ApproachFilePrinter getApproachFilePrinter() {
+    return approachFilePrinter;
   }
 
-  public void setApproachPrinter(ApproachPrinter approachPrinter) {
-    this.approachPrinter = approachPrinter;
+  public void setApproachFilePrinter(ApproachFilePrinter approachFilePrinter) {
+    this.approachFilePrinter = approachFilePrinter;
   }
 
 }
--- trunk/generator/src/de/jdufner/sudoku/generator/text/ApproachFilePrinterImpl.java
+++ trunk/generator/src/de/jdufner/sudoku/generator/text/ApproachFilePrinterImpl.java
@@ -2,24 +2,24 @@
 
 /*
  * Gudoku (http://sourceforge.net/projects/gudoku)
- * Sudoku-Implementierung auf Basis des Google Webtoolkit 
- * (http://code.google.com/webtoolkit/). Die Lösungsalgorithmen in Java laufen 
+ * Sudoku-Implementierung auf Basis des Google Webtoolkit
+ * (http://code.google.com/webtoolkit/). Die Lösungsalgorithmen in Java laufen
  * parallel. Die Sudoku-Rätsel werden mittels JDBC in einer Datenbank
  * gespeichert.
- * 
+ *
  * Copyright (C) 2008 Jürgen Dufner
  *
- * Dieses Programm ist freie Software. Sie können es unter den Bedingungen der 
- * GNU General Public License, wie von der Free Software Foundation 
- * veröffentlicht, weitergeben und/oder modifizieren, entweder gemäß Version 3 
+ * Dieses Programm ist freie Software. Sie können es unter den Bedingungen der
+ * GNU General Public License, wie von der Free Software Foundation
+ * veröffentlicht, weitergeben und/oder modifizieren, entweder gemäß Version 3
  * der Lizenz oder (nach Ihrer Option) jeder späteren Version.
  *
- * Die Veröffentlichung dieses Programms erfolgt in der Hoffnung, daß es Ihnen 
- * von Nutzen sein wird, aber OHNE IRGENDEINE GARANTIE, sogar ohne die 
- * implizite Garantie der MARKTREIFE oder der VERWENDBARKEIT FÜR EINEN 
+ * Die Veröffentlichung dieses Programms erfolgt in der Hoffnung, daß es Ihnen
+ * von Nutzen sein wird, aber OHNE IRGENDEINE GARANTIE, sogar ohne die
+ * implizite Garantie der MARKTREIFE oder der VERWENDBARKEIT FÜR EINEN
  * BESTIMMTEN ZWECK. Details finden Sie in der GNU General Public License.
  *
- * Sie sollten ein Exemplar der GNU General Public License zusammen mit diesem 
+ * Sie sollten ein Exemplar der GNU General Public License zusammen mit diesem
  * Programm erhalten haben. Falls nicht, siehe <http://www.gnu.org/licenses/>.
  *
  */
@@ -56,8 +56,8 @@ public final class ApproachFilePrinterImpl implements ApproachFilePrinter {
   }
 
   @Override
-  public void openFile(final int sudokuId) throws IOException {
-    final File file = new File(DIR, sudokuId + ".txt.gz");
+  public void openFile(final String date) throws IOException {
+    final File file = new File(DIR, "Sudoku_" + date + ".txt.gz");
     final FileOutputStream fos = new FileOutputStream(file);
     final GZIPOutputStream gos = new GZIPOutputStream(fos);
     pw = new PrintWriter(gos);
