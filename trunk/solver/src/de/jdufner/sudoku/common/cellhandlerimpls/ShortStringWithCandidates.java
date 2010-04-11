8b14552969b4a6332f139a92a62f185505d3db97
jdufner <jdufner@99c627dd-ee85-4370-9803-91ec055755ba>
2010-04-11 21:25:32 +0000

    Sudoku-String-Format geändert für unterschiedliche Spielweisen

--- trunk/solver/src/de/jdufner/sudoku/common/cellhandlerimpls/ShortStringWithCandidates.java
+++ trunk/solver/src/de/jdufner/sudoku/common/cellhandlerimpls/ShortStringWithCandidates.java
@@ -2,24 +2,24 @@
 
 /*
  * Gudoku (http://sourceforge.net/projects/gudoku)
- * Sudoku-Implementierung auf Basis des Google Webtoolkit 
- * (http://code.google.com/webtoolkit/). Die Lösungsalgorithmen in Java laufen 
+ * Sudoku-Implementierung auf Basis des Google Webtoolkit
+ * (http://code.google.com/webtoolkit/). Die Lösungsalgorithmen in Java laufen
  * parallel. Die Sudoku-Rätsel werden mittels JDBC in einer Datenbank
  * gespeichert.
- * 
+ *
  * Copyright (C) 2008 Jürgen Dufner
  *
- * Dieses Programm ist freie Software. Sie können es unter den Bedingungen der 
- * GNU General Public License, wie von der Free Software Foundation 
- * veröffentlicht, weitergeben und/oder modifizieren, entweder gemäß Version 3 
+ * Dieses Programm ist freie Software. Sie können es unter den Bedingungen der
+ * GNU General Public License, wie von der Free Software Foundation
+ * veröffentlicht, weitergeben und/oder modifizieren, entweder gemäß Version 3
  * der Lizenz oder (nach Ihrer Option) jeder späteren Version.
  *
- * Die Veröffentlichung dieses Programms erfolgt in der Hoffnung, daß es Ihnen 
- * von Nutzen sein wird, aber OHNE IRGENDEINE GARANTIE, sogar ohne die 
- * implizite Garantie der MARKTREIFE oder der VERWENDBARKEIT FÜR EINEN 
+ * Die Veröffentlichung dieses Programms erfolgt in der Hoffnung, daß es Ihnen
+ * von Nutzen sein wird, aber OHNE IRGENDEINE GARANTIE, sogar ohne die
+ * implizite Garantie der MARKTREIFE oder der VERWENDBARKEIT FÜR EINEN
  * BESTIMMTEN ZWECK. Details finden Sie in der GNU General Public License.
  *
- * Sie sollten ein Exemplar der GNU General Public License zusammen mit diesem 
+ * Sie sollten ein Exemplar der GNU General Public License zusammen mit diesem
  * Programm erhalten haben. Falls nicht, siehe <http://www.gnu.org/licenses/>.
  *
  */
@@ -38,7 +38,8 @@ import de.jdufner.sudoku.common.board.Literal;
 public final class ShortStringWithCandidates implements CellHandler {
 
   private final static char CELL_SEPARATOR = ',';
-  private final static char CANDIDATE_SEPARATOR = '-';
+  private final static char CANDIDATE_BEGIN = '(';
+  private final static char CANDIDATE_END = ')';
 
   private transient StringBuilder stringBuilder = null;
 
@@ -55,14 +56,11 @@ public final class ShortStringWithCandidates implements CellHandler {
     if (cell.isFixed()) {
       stringBuilder.append(cell.getValue());
     } else {
-      int i = 1;
+      stringBuilder.append(CANDIDATE_BEGIN);
       for (Literal candidate : cell.getCandidates()) {
         stringBuilder.append(candidate);
-        if (i < cell.getCandidates().size()) {
-          stringBuilder.append(CANDIDATE_SEPARATOR);
-          i++;
-        }
       }
+      stringBuilder.append(CANDIDATE_END);
     }
   }
 
